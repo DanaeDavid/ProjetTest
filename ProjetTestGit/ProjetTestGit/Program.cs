987a63324875282c85e890f363c21df44a4257f4
Admin <Admin@STAGIAIRE-PC>
2016-11-22 15:08:44 +0100

    visualisation

---
+++ ProjetTestGit/ProjetTestGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetTestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beep");
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
